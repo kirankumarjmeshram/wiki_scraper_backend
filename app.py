3a0afc85233f846c33c1278955f5af6f071f974b
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2023-10-05 13:31:04 +0530

    added paragrap scrapping

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import requests
 from flask_pymongo import PyMongo
 import os
 from dotenv import load_dotenv
-from flask_cors import CORS 
+from flask_cors import CORS
 from bson import ObjectId
 
 load_dotenv()
@@ -20,29 +20,33 @@ app.config['MONGO_URI'] = mongoURI
 mongo = PyMongo(app)
 collection = mongo.db.scraped_data
 
-# POST 
-@app.route('/scrape', methods=['POST'])
+# POST
+@app.route('/scrapeurl', methods=['POST'])
 def scrape_wikipedia():
     try:
         data = request.get_json()
         url = data['url']
-        
+
         # Scrape Wikipedia page
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
-        
-        # Extract all URLs from the page starting with "https"
+
+         # Extract all URLs from the page starting with "https"
         scraped_urls = [a['href'] for a in soup.find_all('a', href=True) if a['href'].startswith('https')]
-        
+
+        # Extract some lines of text from the page
+        paragraphs = soup.find_all('p')
+        paradata = [p.get_text() for p in paragraphs]
+
         # Store the scraped data in MongoDB
-        inserted_data = collection.insert_one({"url": url, "scraped_urls": scraped_urls})
-        
+        inserted_data = collection.insert_one({"url": url,"scraped_urls":scraped_urls, "paradata": paradata})
+
         return jsonify({"message": "Scraping and storing complete!", "id": str(inserted_data.inserted_id)})
     except Exception as e:
         return jsonify({"error": str(e)})
-    
+
 # GET by url
-@app.route('/get_urls', methods=['GET']) 
+@app.route('/getbyurl', methods=['GET']) 
 def get_scraped_urls_single():
     try:
         url_param = request.args.get('url')
@@ -54,14 +58,14 @@ def get_scraped_urls_single():
             return jsonify({"message": "No scraped URLs found for the specified URL."})
     except Exception as e:
         return jsonify({"error": str(e)})
-    
+
 # Get All
-@app.route('/get_all_data', methods=['GET'])
+@app.route('/getalldata', methods=['GET'])
 def get_all_data_reverse():
     try:
-        # get the all data reverse the order
+        # get all data in reverse order
         all_data = list(collection.find().sort("_id", -1))
-        
+
         for item in all_data:
             item["_id"] = str(item["_id"])
 
@@ -71,11 +75,9 @@ def get_all_data_reverse():
             return jsonify({"message": "No data found in the collection."})
     except Exception as e:
         return jsonify({"error": str(e)})
-    
+
 @app.route("/")
 def hello_world():
-    mongo.db.flaskdb.insert_one({"b":"Hello World"})
+    mongo.db.flaskdb.insert_one({"b": "Hello World"})
     return "<p>Wiki Scraper API</p>"
 
-# if __name__ == '__main__':
-#     app.run(debug=True, port=5002)
